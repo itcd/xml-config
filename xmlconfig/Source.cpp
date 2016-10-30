dc8a7f4530ce76a4826564efb0e651d3d34cae3d
Joe (Hasee) <lsz2006.ad@gmail.com>
2016-10-30 10:08:50 +0000

    edit comment

--- xmlconfig/Source.cpp
+++ xmlconfig/Source.cpp
@@ -7,10 +7,12 @@ int main(int argc, char *argv[])
 	ClientConfig cc1;
 	std::cout << cc1.toString() << std::endl;
 
+	// Load settings from the XML file specified in the first command-line argument
 	std::string xml = argc > 1 ? argv[1] : (base_filename(argv[0]) + "_VRServer.xml");
 	ServerConfig sc(xml.c_str());
 	std::cout << xml << std::endl << sc.toString() << std::endl;
 
+	// Load settings from the XML file specified in the first command-line argument
 	std::string xml2 = argc > 2 ? argv[2] : (base_filename(argv[0]) + "_VRClient.xml");
 	ClientConfig cc(xml2.c_str());
 	std::cout << xml2 << std::endl << cc.toString() << std::endl;
